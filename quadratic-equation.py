46ce4ee34a9d66640ba211cff4e1bb3e0d76d148
taogre <mr.tao.gre@gmail.com>
2017-03-21 15:54:27 +0100

    Create quadratic-equation.py

---
+++ quadratic-equation.py
@@ -0,0 +1,19 @@
+import math
+
+a = eval(input("Choose parameter a."))
+b = eval(input("Choose parameter b."))
+c = eval(input("Choose parameter c."))
+
+d = (b**2)-4*a*c
+
+if d>0:
+  print("No")
+elif d==0: 
+  z = math.sqrt(d**2)
+  x = (-b+math.sqrt(z))/2*a
+  print(x)
+else:
+  z = math.sqrt(d**2)
+  x1 = (-b+math.sqrt(z))/2*a
+  x2 = (-b-math.sqrt(z))/2*a
+  print(x1,x2)
